3fae3f0296772f62793ff8f0c05d9a9acaa1e1e6
ncaq <ncaq@ncaq.net>
2018-09-19 17:35:07 +0900

    added: initial commit

---
+++ config.py
@@ -0,0 +1,58 @@
+﻿import sys
+import os
+import datetime
+
+import pyauto
+from keyhac import *
+
+
+def configure(keymap):
+    keymap.editor = "emacsclient.exe"
+
+    # --------------------------------------------------------------------
+    # Customizing the display
+
+    # Font
+    keymap.setFont("MS Gothic", 12)
+
+    # Theme
+    keymap.setTheme("black")
+
+    # --------------------------------------------------------------------
+
+    keymap_global = keymap.defineWindowKeymap()
+
+    # Clipboard history related
+    # Open the clipboard history list
+    keymap_global["W-z"] = keymap.command_ClipboardList
+
+    keymap_firefox = keymap.defineWindowKeymap(
+        exe_name="firefox.exe", class_name="MozillaWindowClass")
+
+    keymap_firefox["C-g"] = "C-Esc"
+    keymap_firefox["C-Slash"] = "C-z"
+
+    keymap_firefox["C-a"] = "Home"
+    keymap_firefox["C-o"] = "C-t"
+    keymap_firefox["A-o"] = "C-S-t"
+    keymap_firefox["C-e"] = "End"
+    keymap_firefox["C-u"] = ["Home", "S-End", "Back"]
+
+    keymap_firefox["C-d"] = "Delete"
+    keymap_firefox["C-h"] = "Left"
+    keymap_firefox["A-h"] = "C-Left"
+    keymap_firefox["C-t"] = "Up"
+    keymap_firefox["C-n"] = "Down"
+    keymap_firefox["C-s"] = "Right"
+    keymap_firefox["A-s"] = "C-Right"
+
+    keymap_firefox["C-q"] = "C-w"
+    keymap_firefox["C-k"] = ["S-End", "C-x"]
+    keymap_firefox["C-x"] = keymap.defineMultiStrokeKeymap("C-X")
+    keymap_firefox["C-x"]["C-g"] = "Esc"
+    keymap_firefox["C-x"]["C-h"] = "C-a"
+    keymap_firefox["C-b"] = "Back"
+    keymap_firefox["A-b"] = "C-Back"
+    keymap_firefox["C-m"] = "Enter"
+    keymap_firefox["C-w"] = "C-x"
+    keymap_firefox["A-w"] = "C-c"
